a4530c0c0563bdbe3a675e51b312f083da1ad43c
marycodes2 <mbeliveau92@gmail.com>
2018-12-19 09:54:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+  let smallestElem = array[0]
+  array.forEach(elem => {
+    if (elem < smallestElem) {
+      smallestElem = elem
+    }
+  }
+)
+  var index = array.indexOf(smallestElem)
+  return array.splice(index, 1)
 }
 
 function insertionSort(array){
-
+  let newArr = []
+  while (array.length > 0) {
+    newArr.push(findMinAndRemove(array)[0])
+  }
+  return newArr
 }
