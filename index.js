9a4c53b2a99d3a375d38ea7f935af69aadc665b8
suttonau <sutton_mjbc@icloud.com>
2019-02-17 05:30:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  //set min to first element
+  //loop through and compare to each element
+  //remove min once found
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  //account for base case. Want unsorted array to be empty
+  let min;
+  let sortedArr = [];
+  while (array.length != 0) {
+    min = findMinAndRemove(array);
+    sortedArr.push(min);
+  }
+  return sortedArr;
 }
